c8bd0a61816d757f63f024f2ca12a8f7415e7378
Joshua Hoblitt <jhoblitt@cpan.org>
2013-12-30 17:05:21 -0700

    add sun_java::1645 class

---
+++ manifests/1645.pp
@@ -0,0 +1,25 @@
+class sun_java::1635 {
+  include sun_java::params
+
+  $package_list = [
+    'jdk-1.6.0_45',
+    'sun-javadb-common-10.6.2',
+    'sun-javadb-demo-10.6.2',
+    'sun-javadb-javadoc-10.6.2',
+    'sun-javadb-client-10.6.2',
+    'sun-javadb-core-10.6.2',
+    'sun-javadb-docs-10.6.2',
+  ]
+
+  $path = "/usr/java/jdk1.6.0_45/bin"
+  $priority = 1645
+
+  class{"sun_java::install":
+   package_list => $package_list, 
+  }
+
+  class{"sun_java::config":
+    path      => $path,
+    priority  => $priority,
+  }
+}
